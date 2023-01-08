a825c7fcd82effa382ae13b5cab1e98294877079
imammalik <leaderkingdt@gmail.com>
2023-01-08 23:13:48 +0700

    test dl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { SlButton, SlDropdown, SlMenu, SlMenuItem } from '@shoelace-style/shoelace/dist/react';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'password-creation';
+
+  ngOninit() {
+    // const container = document.querySelector('.dropdown-selection');
+    // let dropdown = null;
+    // if (container) {
+    //   dropdown = container.querySelector('sl-dropdown');
+    // }
+
+    // if (dropdown) {
+    //   dropdown.addEventListener('sl-select', event => {
+    //     // const selectedItem = event.detail.item;
+    //     // console.log(selectedItem.value);
+    //     console.log(event);
+    //   });
+    // }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.88/dist/');
 
 if (environment.production) {
   enableProdMode();
